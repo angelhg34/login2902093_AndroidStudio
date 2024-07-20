f98c39206cb572641f025edcb84d0d128eaed805
Miguel Angel Ortiz Neira <miguel1516q6@gmail.com>
2024-07-20 18:40:50 -0500

    feat:creando una pantalla de registro signupscreen

---
+++ app/src/main/java/co/edu/sena/login2902093/screens/SignUpScreen.kt
@@ -0,0 +1,26 @@
+package co.edu.sena.login2902093.screens
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.sena.login2902093.componentes.NormalTextComponent
+
+@Composable
+fun SignUpScreen(){
+    Surface(
+        color = Color.White,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+    }
+}
+
+
+@Preview
+@Composable
+fun SignUpScreenPreview() {
+    SignUpScreen()
+}

9255652e433582872881629f4732800fcf87d1d1
Miguel Angel Ortiz Neira <miguel1516q6@gmail.com>
2024-07-20 18:39:33 -0500

    feat:Creando un componente llamado normaltextcomponent

---
+++ app/src/main/java/co/edu/sena/login2902093/componentes/AppComonents.kt
@@ -0,0 +1,25 @@
+package co.edu.sena.login2902093.componentes
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun NormalTextComponent(value:String){
+    Text(
+        text = value,
+        modifier= Modifier.fillMaxWidth().heightIn(min = 80.dp),
+        style = TextStyle(
+            fontSize = 18.sp,
+            fontWeight = FontWeight.Normal,
+            fontStyle = FontStyle.Normal
+        )
+        )
+}

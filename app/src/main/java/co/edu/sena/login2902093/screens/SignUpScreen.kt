a2babbfe983cb1cfd0ebdab4c8b30cc9b5daa0af
Miguel Angel Ortiz Neira <miguel1516q6@gmail.com>
2024-07-20 22:39:25 -0500

    feat and fix: Llamamos el headingtextcomponent para que se muestre el segundo texto, y corregimos el tamaño de letra en cada texto

--- app/src/main/java/co/edu/sena/login2902093/screens/SignUpScreen.kt
+++ app/src/main/java/co/edu/sena/login2902093/screens/SignUpScreen.kt
@@ -1,19 +1,35 @@
 package co.edu.sena.login2902093.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import co.edu.sena.login2902093.R
+import co.edu.sena.login2902093.componentes.HeadingTextComponent
 import co.edu.sena.login2902093.componentes.NormalTextComponent
 
 @Composable
 fun SignUpScreen(){
     Surface(
         color = Color.White,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(28.dp)
     ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+
+            NormalTextComponent(value = stringResource(id = R.string.hello))
+            HeadingTextComponent(value = stringResource(id = R.string.create_account))
+
+        }
 
     }
 }
